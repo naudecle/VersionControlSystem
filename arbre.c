6cafe36acf0151f13419acc1c2de1f78694d64f2
naudecle <naudecle@gmail.com>
2022-03-27 20:44:46 +0400

    Creation de la fonction create pour le pointeur

--- arbre.c
+++ arbre.c
@@ -10,6 +10,27 @@ struct noeud{
 typedef struct noeud Noeud;
 typedef Noeud *NoeudPtr;
 
+NoeudPtr create();
 
 
 int main{}
+
+NoeudPtr create(){
+    int x;
+	NoeudPtr newPtr = malloc(sizeof(Noeud));
+	
+	printf("Entrer un nombre positif(-1 pour stopper la succession): ",x);
+	scanf("%d", &x);
+	if(x == -1){
+		return 0;
+	}
+	else{
+		newPtr->parent = x;
+	    printf("Entrer la valeur du fils gauche de %d \n",x);
+	    newPtr->gauche = create();
+	    printf("Entrer la valeur du fils droit de %d \n", x);
+	    newPtr->droit = create();
+	    return newPtr;
+	}
+
+}
