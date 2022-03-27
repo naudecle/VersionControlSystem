f8dcfc77ae99c66ff59907415e2877fb97ba44f6
naudecle <naudecle@gmail.com>
2022-03-27 17:45:00 +0400

    Creation du pointeur et de la structure

--- arbre.c
+++ arbre.c
@@ -1,1 +1,15 @@
-main{}
+#include<stdio.h>
+#include<stdlib.h>
+
+struct noeud{
+    int parent;
+    struct noeud *droit;
+    struct noeud *gauche;
+}
+
+typedef struct noeud Noeud;
+typedef Noeud *NoeudPtr;
+
+
+
+int main{}
